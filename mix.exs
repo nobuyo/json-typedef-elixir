92e065f53b67fd56a56ce2b1f562b23fc1e29141
nobuyo <world_fantasy_2017@live.jp>
2021-01-23 16:35:05 +0000

    update metadata

--- mix.exs
+++ mix.exs
@@ -1,28 +1,49 @@
 defmodule JTD.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/nobuyo/json-typedef-elixir"
+  @version "0.1.0"
+
   def project do
     [
       app: :jtd,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.11",
+      deps: deps(),
+      description: description(),
+      package: package(),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      source_url: @source_url,
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
+  defp description do
+    """
+    Elixir implementation of JSON Type Definition validation.
+    """
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      maintainers: ["Nobuo Takizawa"],
+      links: %{
+        "GitHub" => @source_url,
+        "RFC" => "https://www.rfc-editor.org/rfc/rfc8927.html"
+      }
+    ]
+  end
+
   defp deps do
     [
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.23", only: :dev, runtime: false},
     ]
   end
 end
